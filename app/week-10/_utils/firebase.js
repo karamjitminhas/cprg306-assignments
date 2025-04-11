86f9aac111d1269eb776d95d4a2129438d497bb6
Karam <karamjitsingh@edu.sait.ca>
2025-04-11 16:26:41 -0600

    fixed exposed secrets

--- app/week-10/_utils/firebase.js
+++ app/week-10/_utils/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDYCKZrDK3HkimMSB2Y8Z0ScXJxL-nPa0o",
-  authDomain: "cprg306-assignments-d3d4d.firebaseapp.com",
-  projectId: "cprg306-assignments-d3d4d",
-  storageBucket: "cprg306-assignments-d3d4d.firebasestorage.app",
-  messagingSenderId: "222313276745",
-  appId: "1:222313276745:web:37bb31ad6c974ba0d9a698"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
